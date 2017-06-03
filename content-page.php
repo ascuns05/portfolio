7fddb11704698d9fa32008ceaa00be4cddc5a160
ascuns <freethinkel@gmail.com>
2017-06-03 19:53:14 +0300

    пофиксил id аватара

--- content-page.php
+++ content-page.php
@@ -1,5 +1,9 @@
+<?php 
+$contacts_id = get_the_ID();
+?>
+
 <div class="content_header">
-	<img src="<?php echo get_the_post_thumbnail_url(8); ?>" alt="Колесников Егор" />
+	<img src="<?php echo get_the_post_thumbnail_url($contacts_id); ?>" alt="<?php echo get_bloginfo('name'); ?>" />
 	<h1><?php echo get_bloginfo('name'); ?></h1>
 	<p><?php echo get_bloginfo('description'); ?></p>
 </div>
